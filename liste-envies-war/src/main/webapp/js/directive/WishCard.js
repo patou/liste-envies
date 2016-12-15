a8e4930a8877ae76b310f5da7f6f003142d79c2e
Patrice <patrice@desaintsteban.fr>
2016-12-15 17:03:19 +0100

    Correction des fonctions Offir et Annuler.

--- liste-envies-war/src/main/webapp/js/directive/WishCard.js
+++ liste-envies-war/src/main/webapp/js/directive/WishCard.js
@@ -148,14 +148,14 @@ var WishCard = function ($scope, envieService) {
 
 
     w.given = function(id) {
-        envieService.give({name:w.name, id:id}, {}, function(updatedData) {
+        envieService.give({name:w.listName, id:id}, {}, function(updatedData) {
             w.parentController.updatePropertiesWish(w.wish, updatedData);
         });
     };
 
 
     w.cancel = function(id) {
-        envieService.cancel({name:w.name, id:id}, {}, function(updatedData) {
+        envieService.cancel({name:w.listName, id:id}, {}, function(updatedData) {
             w.parentController.updatePropertiesWish(w.wish, updatedData);
         });
     };
