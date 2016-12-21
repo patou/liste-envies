caefc5f2ea411862e7b32ab7b059be2ce439042d
Emmanuel de Saint Steban <emmanueldesaintsteban@gmail.com>
2016-12-21 17:26:31 +0100

    mise à jour graphique + nouveau composant pour afficher une page

--- liste-envies-war/src/main/webapp/js/directive/pageDirective.js
+++ liste-envies-war/src/main/webapp/js/directive/pageDirective.js
@@ -5,15 +5,55 @@
  * @description
  *
  *
- * @restrict A
  * */
 angular.module('ListeEnviesDirectives')
-    .directive('page', function () {
-        return {
-            restrict: 'E',
-            transclude: true,
-            link: function (scope, elem, attr) {
+    .component('page', {
+        templateUrl: 'templates/directive/pageDirective.html',
+        controller: PagesDirectivesController,
+        controllerAs: 'main',
+        transclude: true,
+        bindings: {
 
-            }
-        };
-});
+        }
+    });
+PagesDirectivesController.$inject = ['$scope', '$http', '$location', 'AuthService', 'UtilitiesServices'];
+function PagesDirectivesController ($scope, $http, $location, AuthService, UtilitiesServices) {
+    AuthService.refresh();
+    var main = this;
+    main.isActive = function(viewLocation) {
+        return viewLocation === $location.path();
+    };
+
+    main.wishLists = null;
+
+    main.user = AuthService.getUser();
+    $scope.$watch(function () { return AuthService.getUser(); }, function () {
+        main.isAuthenticated = AuthService.isAuthenticated();
+        main.isAdmin = AuthService.isAdmin();
+        main.user = AuthService.getUser();
+
+        if (main.isAuthenticated) {
+            UtilitiesServices.getList().then(function (data) {
+                main.wishLists = data;
+            });
+        } else {
+            main.wishLists = null;
+        }
+
+    });
+
+    main.goList = function (name) {
+        $location.url("/"+name);
+    };
+
+    main.loginPath = function() {
+        return "/login?path=" + $location.path();
+    };
+    main.logoutPath = function() {
+        return "/logout?path=" + $location.path();
+    };
+
+    loadMaterialsKits();
+
+
+}
