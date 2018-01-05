f7ca932d4ea0fbc0859da08e7460bcd4b1b1b5b3
Patrice de Saint Steban <patrice@desaintsteban.fr>
2018-01-05 17:05:34 +0100

    Ne passe les notifications à lue qu'au bout de deux heures.

--- liste-envies-war/src/main/java/fr/desaintsteban/liste/envies/servlet/GetUserServlet.java
+++ liste-envies-war/src/main/java/fr/desaintsteban/liste/envies/servlet/GetUserServlet.java
@@ -1,6 +1,7 @@
 package fr.desaintsteban.liste.envies.servlet;
 import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +15,20 @@ public class GetUserServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("application/json");
 		AppUser userAuthenticated = ServletUtils.getUserAuthenticated();
-		userAuthenticated.setLastNotification(userAuthenticated.getLastVisit());
+		if (getDifferenceHours(userAuthenticated.getLastNotification(), userAuthenticated.getLastVisit()) > 2) {
+			userAuthenticated.setLastNotification(userAuthenticated.getLastVisit());
+		}
 		userAuthenticated.setLastVisit(new Date());
 		AppUserService.createOrUpdate(userAuthenticated);
 		resp.getWriter().println(ServletUtils.toJson(userAuthenticated));
+	}
+
+	private static long getDifferenceHours(Date d1, Date d2) {
+		if (d1 == null)
+			d1 = new Date();
+		if (d2 == null)
+			d2 = new Date();
+		long diff = d2.getTime() - d1.getTime();
+		return TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 }
