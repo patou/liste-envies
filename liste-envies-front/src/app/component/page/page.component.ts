d78dcfd94f76ff408d49735d98ea50d6fd43835c
manudss <emmanueldesaintsteban@gmail.com>
2018-04-11 22:40:54 +0200

    wip : error compile

--- liste-envies-front/src/app/component/page/page.component.ts
+++ liste-envies-front/src/app/component/page/page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import {AuthService} from '../../service/auth.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-page',
@@ -8,10 +9,13 @@ import {AuthService} from '../../service/auth.service';
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
+  public userAuth: Observable<firebase.User>;
+
 
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.userAuth = this.auth.user;
   }
 
   loginGoogle() {
