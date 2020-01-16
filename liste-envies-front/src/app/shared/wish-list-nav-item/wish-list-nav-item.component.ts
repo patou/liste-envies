18d8aa611ac2703d47b998a6e3d9631e5fa44891
manudss <emmanueldesaintsteban@gmail.com>
2020-01-16 23:34:26 +0100

    :lipstick: style(wish-list-nav) proposal to display menu items lists

---
+++ liste-envies-front/src/app/shared/wish-list-nav-item/wish-list-nav-item.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { WishList } from "../../models/WishList";
+import { NotificationsQuery } from "../../state/app/notifications.query";
+import { Observable } from "rxjs/Observable";
+
+@Component({
+  selector: "wish-list-nav-item",
+  templateUrl: "./wish-list-nav-item.component.html",
+  styleUrls: ["./wish-list-nav-item.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class WishListNavItemComponent implements OnInit {
+  @Input()
+  wishlist: WishList;
+
+  @Input()
+  active: boolean = false;
+
+  notifsCount$: Observable<number>;
+
+  constructor(private notificationQuery: NotificationsQuery) {}
+
+  ngOnInit() {
+    this.notifsCount$ = this.notificationQuery.selectCount(
+      notification => notification.listId === this.wishlist.name
+    );
+  }
+}
