9170f1a9ae1197a63331b2e49fd0fed882530ae8
Patrice de Saint Steban <patrice@desaintsteban.fr>
2020-11-08 23:50:07 +0100

    Revert ":fire: Remove deprecated Entity"
    This reverts commit 19d0bf7b

--- liste-envies-war/src/main/java/fr/desaintsteban/liste/envies/service/OfyService.java
+++ liste-envies-war/src/main/java/fr/desaintsteban/liste/envies/service/OfyService.java
@@ -8,12 +8,18 @@ import fr.desaintsteban.liste.envies.model.AppUser;
 import fr.desaintsteban.liste.envies.model.Notification;
 import fr.desaintsteban.liste.envies.model.Wish;
 import fr.desaintsteban.liste.envies.model.WishList;
+import fr.desaintsteban.liste.envies.model.deprecated.Envy;
+import fr.desaintsteban.liste.envies.model.deprecated.ListEnvies;
 
 public final class OfyService {
     static {
     	factory().register(AppUser.class);
         factory().register(Wish.class);
         factory().register(WishList.class);
+        // todo delete when migrated
+        factory().register(Envy.class);
+        factory().register(ListEnvies.class);
+
         factory().register(Notification.class);
     }
 
