abc1e97087be769dc557ae6f8ba7be8bf26d3364
Patrice De Saint Steban <patrice@desaintsteban.fr>
2026-01-30 23:57:46 +0000

    refactor: simplifie les importations des plugins FilePond dans le module partagé

--- liste-envies-front/src/app/shared/shared.module.ts
+++ liste-envies-front/src/app/shared/shared.module.ts
@@ -25,9 +25,6 @@ import { TruncatePipe } from "./pipes/truncate.pipe";
 import { AvatarComponent } from "./avatar/avatar.component";
 import { WishListItemComponent } from "./wish-list-item/wish-list-item.component";
 import { RouterModule } from "@angular/router";
-import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
-import { provideAuth, getAuth } from "@angular/fire/auth";
-import { provideFirestore, getFirestore } from "@angular/fire/firestore";
 import { ImgFormComponent } from "./img-form/img-form.component";
 import { ReadMoreComponent } from "./read-more/read-more.component";
 import { PageFooterComponent } from "./page-footer/page-footer.component";
@@ -37,11 +34,11 @@ import { FilePondModule, registerPlugin } from "ngx-filepond";
 
 /*import { Ng2GridDirective } from './ng2-grid/ng2-grid.directive';*/
 
-import * as FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import * as FilePondPluginFileEncode from "filepond-plugin-file-encode";
-import * as FilePondPluginImageResize from "filepond-plugin-image-resize";
-import * as FilePondPluginImageTransform from "filepond-plugin-image-transform";
-import * as FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginFileEncode from "filepond-plugin-file-encode";
+import FilePondPluginImageResize from "filepond-plugin-image-resize";
+import FilePondPluginImageTransform from "filepond-plugin-image-transform";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import { WishListNavItemComponent } from "./wish-list-nav-item/wish-list-nav-item.component";
 import { SideOfWishComponent } from "../component/side_of_wish/side-of-wish.component";
 import { AppModule } from "../app.module";
