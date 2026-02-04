93d827417eb404ed14ede7442bb59598ec881c1d
Patrice de Saint Steban <patrice@desaintsteban.fr>
2026-02-04 22:58:39 +0100

    feat: Introduce a new 'recette' environment and configure its build and CI/CD deployment.

---
+++ liste-envies-front/src/environments/environment.recette.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBNWwcACaC1d8tpWmLZGdO48u3MVq8vx2M",
+    authDomain: "test-liste-envies.firebaseapp.com",
+    databaseURL: "https://test-liste-envies.firebaseio.com",
+    projectId: "test-liste-envies",
+    storageBucket: "test-liste-envies.firebasestorage.app",
+    messagingSenderId: "424684618174",
+    appId: "1:424684618174:web:a300cf545f9f87b7cd79ef"
+  },
+  hmr: false
+};
